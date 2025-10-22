4de054dcc48ed4d21bb94ca37620942f5b43d1be
icysamon <github@icysamon.com>
2025-10-22 23:41:53 +0900

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -40,7 +40,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            フリーBGM素材
+            自作無料BGM素材
           </Link>
         </div>
         <div className="flex">
