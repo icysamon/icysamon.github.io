694e8c48ec6fe0be172d475ca771e270493622e5
icysamon <github@icysamon.com>
2025-11-21 17:40:19 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,16 +38,22 @@ export default function Home() {
           <h2 className={h2Style}>作曲・編曲</h2>
           <div className={divStyle}>
             <Card
+              image="https://image.icysamon.com/index/2025/11/dreaming-fish.webp"
+              href="https://linkco.re/QaGRyDV3"
+              title="サカナノユメ"
+              date="2025年11月21日"
+            />
+            <Card
               image="https://image.icysamon.com/index/2025/11/to-you-in-2020.webp"
               href="https://linkco.re/ad8AsbYr"
               title="2020年のあなたへ"
-              date="	2025年11月15日"
+              date="2025年11月15日"
             />
             <Card
               image="https://image.icysamon.com/index/2025/04/light-year.webp"
               href="https://linkco.re/tGbPetG4"
               title="光年"
-              date="	2025年4月2日"
+              date="2025年4月2日"
             />
             <Card
               image="https://image.icysamon.com/index/2025/03/beginning-of-the-journey.webp"
